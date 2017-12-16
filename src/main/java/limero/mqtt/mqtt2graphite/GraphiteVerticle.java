dc0d39a36d426e817c08440e7522e6e81e3f1c16
vortex314 <lieven.merckx@gmail.com>
2017-12-16 23:14:46 +0100

    configurable version

--- src/main/java/limero/mqtt/mqtt2graphite/GraphiteVerticle.java
+++ src/main/java/limero/mqtt/mqtt2graphite/GraphiteVerticle.java
@@ -23,9 +23,14 @@ public class GraphiteVerticle extends AbstractVerticle {
 	long _messagesReceived = 0;
 	long _lastMessagesReceivedTime;
 	long _lastMessagesReceivedSample;
+/* ==================================================  CONFIG */	
+	String _metricsPrefix;
+	int _metricsInterval;
 	String _host;
 	int _port;
-
+	String _subscribeTo;
+	String _publishMetricsTo;
+/* ==========================================================*/
 	@Override
 	public void init(Vertx vertx, Context context) {
 		super.init(vertx, context);
@@ -34,6 +39,9 @@ public class GraphiteVerticle extends AbstractVerticle {
 	void SocketConnect() {
 		_host = config().getString("host", "limero.ddns.net");
 		_port = config().getInteger("port", 1883);
+		_publishMetricsTo = config().getString("publishMetricsTo", "metrics");
+		_subscribeTo = config().getString("subscribeTo", "metrics");
+		
 		client = vertx.createNetClient(options);
 		client.connect(_port, _host, res -> {
 			if (res.succeeded()) {
@@ -47,14 +55,26 @@ public class GraphiteVerticle extends AbstractVerticle {
 			}
 		});
 	}
+	
+	void reportMetric(String name,double value,long time){
+		JsonObject json = new JsonObject();
+		json.put("time", time);
+		json.put("metric", name);
+		json.put("value", value);
+		eb.publish(_publishMetricsTo, json);
+	}
 
 	void reportMetrics() {
 		_lastMessagesReceivedSample = System.currentTimeMillis();
 		_messagesReceived = 0;
 		vertx.setPeriodic(3000, id -> {
 			long now = System.currentTimeMillis();
-			double timeDelta = now - _lastMessagesReceivedTime;
-			double messagesDelta = (_messagesReceived - _lastMessagesReceivedSample) * 1000.0 / timeDelta;
+			double timeDelta = (now - _lastMessagesReceivedTime)/1000.0;
+			double messagesDelta = (_messagesReceived - _lastMessagesReceivedSample) / timeDelta;
+			
+			reportMetric(_metricsPrefix+"messagesSend",_messagesReceived,System.currentTimeMillis()/1000);
+
+			
 			log.info(" Graphite messages send : " + _messagesReceived + " " + String.format("%7.2f", messagesDelta)
 					+ " msg/sec ");
 			_lastMessagesReceivedSample = _messagesReceived;
@@ -64,10 +84,17 @@ public class GraphiteVerticle extends AbstractVerticle {
 
 	// Called when verticle is deployed
 	public void start() {
+		
+		_host = config().getString("host", "limero.ddns.net");
+		_port = config().getInteger("port",1883);
+		_metricsPrefix = config().getString("metricsPrefix", "src/mqtt2graphite/");
+		_metricsInterval = config().getInteger("metricsInterval",5000);
+		_subscribeTo =config().getString("subscribeTo","metrics");
+		
 		eb = vertx.eventBus();
 		SocketConnect();
 
-		eb.consumer("graphite", message -> {
+		eb.consumer(_subscribeTo, message -> {
 			log.debug("I have received a message: " + message.body());
 			JsonObject json = (JsonObject) message.body();
 			if (socket != null) {
--- src/main/java/limero/mqtt/mqtt2graphite/MqttVerticle.java
+++ src/main/java/limero/mqtt/mqtt2graphite/MqttVerticle.java
@@ -1,5 +1,8 @@
 package limero.mqtt.mqtt2graphite;
 
+import java.util.List;
+import java.util.Vector;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
@@ -7,6 +10,7 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Context;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.mqtt.MqttClient;
 import io.vertx.mqtt.MqttClientOptions;
@@ -15,13 +19,21 @@ public class MqttVerticle extends AbstractVerticle {
 	MqttClient client;
 	EventBus eb;
 	static Logger log = LogManager.getLogger(MqttVerticle.class.getSimpleName());
+	
+//===============================================  CONFIG
 	String _host;
 	int _port;
+	JsonArray _subscribe;
+	String _publishTo;
 	// Called when verticle is deployed
 
 	long _messagesReceived = 0;
+	long _numbersReceived=0;
+	
 	long _lastMessagesReceivedTime;
 	long _lastMessagesReceivedSample;
+	String _metricsPrefix;
+	int _metricsInterval;
 	
 	@Override
 	public void init(Vertx vertx, Context context) {
@@ -41,6 +53,14 @@ public class MqttVerticle extends AbstractVerticle {
 	void MqttConnect() {
 		_host = config().getString("host", "limero.ddns.net");
 		_port = config().getInteger("port",1883);
+		_metricsPrefix = config().getString("metricsPrefix", "src/mqtt2graphite/");
+		_metricsInterval = config().getInteger("metricsInterval",5000);
+		_publishTo = config().getString("publishTo", "metrics");
+
+		List<String> defaultSubscribe=new Vector<String>();
+		defaultSubscribe.add("#");
+		_subscribe =  config().getJsonArray("subscribe",new JsonArray(defaultSubscribe));
+		
 		log.info(" Mqtt connecting to " + _host + ":" + _port + ".");
 
 		MqttClientOptions options = new MqttClientOptions();
@@ -62,13 +82,10 @@ public class MqttVerticle extends AbstractVerticle {
 					log.debug(" => " + msg.topicName() + ":" + msg.payload().toString());
 					Double d = getNumber(msg.payload().toString()); 
 					if (d != null) {
+						_numbersReceived++;
 						String str = msg.topicName();
 						String metric = str.replaceAll("/", ".");
-						JsonObject json = new JsonObject();
-						json.put("time", System.currentTimeMillis() / 1000);
-						json.put("metric", metric);
-						json.put("value", d);
-						eb.publish("graphite", json);
+						reportMetric(metric,d,System.currentTimeMillis() / 1000);
 					}
 
 				});
@@ -80,14 +97,28 @@ public class MqttVerticle extends AbstractVerticle {
 		});
 	}
 
+	void reportMetric(String name,double value,long time){
+		JsonObject json = new JsonObject();
+		json.put("time", time);
+		json.put("metric", name);
+		json.put("value", value);
+		eb.publish(_publishTo, json);
+	}
+	
 	void reportMetrics() {
 		_lastMessagesReceivedSample = System.currentTimeMillis();
 		_messagesReceived = 0;
-		long timerID = vertx.setPeriodic(3000, id -> {
+		long timerID = vertx.setPeriodic(_metricsInterval, id -> {
 			long now = System.currentTimeMillis();
 			double timeDelta = now - _lastMessagesReceivedTime;
 			double messagesDelta = (_messagesReceived - _lastMessagesReceivedSample) * 1000 / timeDelta;
+			
+			reportMetric(_metricsPrefix+"messagesReceived",_messagesReceived,System.currentTimeMillis()/1000);
+			reportMetric(_metricsPrefix+"numberReceived",_numbersReceived,System.currentTimeMillis()/1000);
+			reportMetric(_metricsPrefix+"messagesPerSec",messagesDelta,System.currentTimeMillis()/1000);
+			
 			log.info(" MQTT messages received : " + _messagesReceived + " " +String.format("%7.2f" , messagesDelta) + " msg/sec ");
+			
 			_lastMessagesReceivedSample = _messagesReceived;
 			_lastMessagesReceivedTime = now;
 		});
